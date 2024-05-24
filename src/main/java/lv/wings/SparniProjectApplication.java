3841293a199b9acf16370d15608aa162f8cf9cac
sethu21 <sethu2196@gmail.com>
2024-05-24 11:51:06 +0300

    Initial commit

---
+++ src/main/java/lv/wings/SparniProjectApplication.java
@@ -0,0 +1,13 @@
+package lv.wings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparniProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SparniProjectApplication.class, args);
+	}
+
+}
