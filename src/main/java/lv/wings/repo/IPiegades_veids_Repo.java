c42f124c3f93bb2ea3e473f8557d689dc545690d
matyss <matissp1302@gmail.com>
2024-05-31 10:45:24 +0300

    Izveidoju Interface repo

---
+++ src/main/java/lv/wings/repo/IPiegades_veids_Repo.java
@@ -0,0 +1,9 @@
+package lv.wings.repo;
+
+import org.springframework.data.repository.CrudRepository;
+
+import lv.wings.model.Piegades_veids;
+
+public interface IPiegades_veids_Repo extends CrudRepository<Piegades_veids, Integer>{
+
+}
---
+++ src/main/java/lv/wings/repo/IPircejs_Repo.java
@@ -0,0 +1,9 @@
+package lv.wings.repo;
+
+import org.springframework.data.repository.CrudRepository;
+
+import lv.wings.model.Pircejs;
+
+public interface IPircejs_Repo extends CrudRepository<Pircejs, Integer>{
+
+}
