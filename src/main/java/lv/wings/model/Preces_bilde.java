423d2985ca4764986016a14e8a5d572052815ef9
sethu21 <sethu2196@gmail.com>
2024-05-31 10:43:52 +0300

    Pievienoju Preces bildes tabulu

--- src/main/java/lv/wings/model/Preces_bilde.java
+++ src/main/java/lv/wings/model/Preces_bilde.java
@@ -1,5 +1,42 @@
 package lv.wings.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@Table(name="PrecesBildeTable")
+@Entity
 public class Preces_bilde {
 
+	@Id
+	@Column(name = "P_b_id")
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Setter(value = AccessLevel.NONE)
+	private int p_b_id;
+	
+	
+	//TODO sis naks no preces tabulas
+	//private int prece_id;
+	
+	@Column(name = "Bilde")
+	private String bilde;
+	
+	@Column(name = "Apraksts")
+	@NotNull
+	@Size(min = 4, max = 150)
+	private String apraksts;
 }
