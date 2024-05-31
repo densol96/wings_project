a065b01a06033450402491548d857fc36ac7226c
Annija Statevica <annija.statevica@gmail.com>
2024-05-31 12:03:45 +0300

    Pievienoti repozitoriji (Kategorijas,Pirkuma elements, Preces, Preces bilde)

---
+++ src/main/java/lv/wings/repo/IPrece_Repo.java
@@ -0,0 +1,9 @@
+package lv.wings.repo;
+
+import org.springframework.data.repository.CrudRepository;
+
+import lv.wings.model.Prece;
+
+public interface IPrece_Repo extends CrudRepository<Prece, Integer>{
+
+}
