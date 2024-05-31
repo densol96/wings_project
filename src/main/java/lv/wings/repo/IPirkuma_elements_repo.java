10b2f5e44e26c7e246488a7f551d714db7205393
Annija Statevica <annija.statevica@gmail.com>
2024-05-31 12:03:45 +0300

    Pievienoti repozitoriji (Kategorijas,Pirkuma elements, Preces, Preces bilde)

---
+++ src/main/java/lv/wings/repo/IPirkuma_elements_repo.java
@@ -0,0 +1,9 @@
+package lv.wings.repo;
+
+import org.springframework.data.repository.CrudRepository;
+
+import lv.wings.model.Pirkuma_elements;
+
+public interface IPirkuma_elements_repo extends CrudRepository<Pirkuma_elements,Integer>{
+
+}
---
+++ src/main/java/lv/wings/repo/IPreces_bilde_Repo.java
@@ -0,0 +1,9 @@
+package lv.wings.repo;
+
+import org.springframework.data.repository.CrudRepository;
+
+import lv.wings.model.Preces_bilde;
+
+public interface IPreces_bilde_Repo extends CrudRepository<Preces_bilde,Integer> {
+
+}
