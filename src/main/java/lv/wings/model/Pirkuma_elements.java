1671c053ccbd4e208dcd851bb94f901e86cb2a87
Annija Statevica <annija.statevica@gmail.com>
2024-05-31 11:17:31 +0300

    Pirkuma_elements model pabeigts

--- src/main/java/lv/wings/model/Pirkuma_elements.java
+++ src/main/java/lv/wings/model/Pirkuma_elements.java
@@ -1,5 +1,36 @@
 package lv.wings.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Min;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@Table(name="PirkumaElementsTable")
+@Entity
 public class Pirkuma_elements {
 
+	@Id
+	@Column(name = "P_e_id")
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Setter(value = AccessLevel.NONE)
+	private int p_e_id;
+	
+	//TODO te nak pirkums ID
+	//TODO te nāk prece ID
+	
+	@Column(name = "Daudzums")
+	@Min(1)
+	private int daudzums;
 }
