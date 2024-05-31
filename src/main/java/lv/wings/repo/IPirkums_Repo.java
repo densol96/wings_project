3aece7eb1dd642256371309a38701645bd1d4057
matyss <matissp1302@gmail.com>
2024-05-31 10:45:24 +0300

    Izveidoju Interface repo

---
+++ src/main/java/lv/wings/repo/IPirkums_Repo.java
@@ -0,0 +1,9 @@
+package lv.wings.repo;
+
+import org.springframework.data.repository.CrudRepository;
+
+import lv.wings.model.Pirkums;
+
+public interface IPirkums_Repo extends CrudRepository<Pirkums, Integer>{
+
+}
---
+++ src/main/java/lv/wings/repo/ISamaksas_veids_Repo.java
@@ -0,0 +1,9 @@
+package lv.wings.repo;
+
+import org.springframework.data.repository.CrudRepository;
+
+import lv.wings.model.Samaksas_veids;
+
+public interface ISamaksas_veids_Repo extends CrudRepository<Samaksas_veids, Integer>{
+
+}
