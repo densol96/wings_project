e192e2fb631c6c86e61f10de1dc45b72c38db923
laurelis <s23kairlaur@venta.lv>
2024-06-22 13:41:15 +0300

    controllers, services, views, css

--- src/main/java/lv/wings/repo/IPasakumaBildeRepo.java
+++ src/main/java/lv/wings/repo/IPasakumaBildeRepo.java
@@ -1,9 +1,14 @@
 package lv.wings.repo;
 
+import java.util.ArrayList;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import lv.wings.model.PasakumaBilde;
 
 public interface IPasakumaBildeRepo extends CrudRepository<PasakumaBilde, Integer>{
-
+	
+	@Query(nativeQuery = true, value = "select pasakumi.idpa, pasakumi.idpaka, pasakumi.idpako, pasakumi.nosaukums, pasakumi.apraksts, pasakumi.vieta, pasakumi.sakumadatums, pasakumi.beigudatums, pasakuma_bildes.idpab, pasakuma_bildes.atsauce_uz_bildi, pasakumi.key_words from pasakumi left join pasakuma_bildes on pasakumi.idpa = pasakuma_bildes.idpa;")
+	ArrayList<PasakumaBilde> findAllPasakumiWithImages();
 }
--- src/main/java/lv/wings/repo/IPasakumsRepo.java
+++ src/main/java/lv/wings/repo/IPasakumsRepo.java
@@ -1,9 +1,22 @@
 package lv.wings.repo;
 
+import java.util.ArrayList;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import lv.wings.model.Pasakums;
 
-public interface IPasakumsRepo extends CrudRepository<Pasakums, Integer>{
+public interface IPasakumsRepo extends CrudRepository<Pasakums, Integer> {
+
+	@Query(nativeQuery = true, value = "select pasakumi.idpa, pasakumi.idpaka, pasakumi.idpako, pasakumi.nosaukums, pasakumi.apraksts, pasakumi.vieta, pasakumi.sakumadatums, pasakumi.beigudatums, pasakuma_bildes.idpab, pasakumi.key_words, pasakuma_bildes.atsauce_uz_bildi from pasakumi left join pasakuma_bildes on pasakumi.idpa = pasakuma_bildes.idpa;")
+	ArrayList<Pasakums> findAllPasakumiWithImages();
+	
+	@Query(nativeQuery = true, value = "select pasakumi.idpa, pasakumi.idpaka, pasakumi.idpako, pasakumi.nosaukums, pasakumi.apraksts, pasakumi.vieta, pasakumi.sakumadatums, pasakumi.beigudatums, pasakuma_bildes.idpab, pasakumi.key_words, pasakuma_bildes.atsauce_uz_bildi from pasakumi left join pasakuma_bildes on pasakumi.idpa = pasakuma_bildes.idpa order by pasakumi.idpa desc;")
+	ArrayList<Pasakums> findAllPasakumiWithImagesDescOrder();
+	
+	@Query(nativeQuery = true, value = "select pasakumi.idpa, pasakumi.idpaka, pasakumi.idpako, pasakumi.nosaukums, pasakumi.apraksts, pasakumi.vieta, pasakumi.sakumadatums, pasakumi.beigudatums, pasakuma_bildes.idpab, pasakumi.key_words, pasakuma_bildes.atsauce_uz_bildi from pasakumi left join pasakuma_bildes on pasakumi.idpa = pasakuma_bildes.idpa order by pasakumi.idpa asc;")
+	ArrayList<Pasakums> findAllPasakumiWithImagesAscOrder();
+
 
 }
