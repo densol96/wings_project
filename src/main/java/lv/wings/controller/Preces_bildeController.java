5a4a4187c0f7f4549293fc236fce907a11eddcec
Annija Statevica <annija.statevica@gmail.com>
2024-06-25 10:05:53 +0300

    Add: Pārbaudīts Show all Kategorijām

--- src/main/java/lv/wings/controller/Preces_bildeController.java
+++ src/main/java/lv/wings/controller/Preces_bildeController.java
@@ -1,5 +1,10 @@
 package lv.wings.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/preces/bilde")
 public class Preces_bildeController {
 
 }
