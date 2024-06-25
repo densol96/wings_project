034526ab84e3345ad69ee5a47c44b23001441d80
Annija Statevica <annija.statevica@gmail.com>
2024-06-25 10:05:53 +0300

    Add: Pārbaudīts Show all Kategorijām

--- src/main/java/lv/wings/controller/KategorijasController.java
+++ src/main/java/lv/wings/controller/KategorijasController.java
@@ -1,5 +1,34 @@
 package lv.wings.controller;
 
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lv.wings.model.Kategorijas;
+import lv.wings.service.IKategorijasService;
+
+@Controller
+@RequestMapping("/kategorijas")
 public class KategorijasController {
+	
+	@Autowired
+	private IKategorijasService kategorijasService;
+	
+	@GetMapping("/show/all") //localhost:8080/kategorijas/show/all
+	public String getAllKategorijas(Model model) {
+		try {
+			ArrayList<Kategorijas> allKategorijas = kategorijasService.retrieveAll();
+			model.addAttribute("mydata", allKategorijas); //padod izfiltrētās kategorijas uz katergorijas-all-page.html
+			model.addAttribute("msg","All kategorijas");
+			return "kategorijas-page";
+		}catch(Exception e) {
+			model.addAttribute("mydata", e.getMessage());//padod kludas zinu uz error-page.html lapu
+			return "error-page";
+		}
+	}
 
 }
--- src/main/java/lv/wings/controller/Pirkuma_elementsController.java
+++ src/main/java/lv/wings/controller/Pirkuma_elementsController.java
@@ -1,5 +1,10 @@
 package lv.wings.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/pirkuma/elements")
 public class Pirkuma_elementsController {
 
 }
