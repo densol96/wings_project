cbd1846f6c7f336088525bd07cdbca539f1ddef0
Annija Statevica <annija.statevica@gmail.com>
2024-06-27 11:04:25 +0300

    Update: Show all pages nņetas filtrācijas

--- src/main/java/lv/wings/controller/Preces_bildeController.java
+++ src/main/java/lv/wings/controller/Preces_bildeController.java
@@ -22,7 +22,6 @@ public class Preces_bildeController {
 	@Autowired
 	private IPreces_bildeService bildeService;
 	
-	
 	@GetMapping("show/all") //localhost:8080/preces/bilde/show/all
 	public String getAllPrecesBildes(Model model) {
 		try {
