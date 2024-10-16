16b6dc16575fbe295aa377cfcf9e31450c838e89
laurelis <lauris-kairo@inbox.lv>
2024-10-16 17:54:36 +0300

    new navbar style, not finished, need to fix

--- front-end-react/tailwind.config.js
+++ front-end-react/tailwind.config.js
@@ -1,8 +1,43 @@
 /** @type {import('tailwindcss').Config} */
+
+const plugin = require("tailwindcss/plugin");
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			textShadow: {
+				sm: "0 1px 1px var(--tw-shadow-color)",
+				DEFAULT: "0 2px 4px var(--tw-shadow-color)",
+				lg: "0 8px 16px var(--tw-shadow-color)",
+			},
+			transitionProperty: {
+				width: "width",
+			},
+
+			fontFamily: {
+				roboto: ["Roboto", "sans-serif"],
+			},
+
+			rotate: {
+				75: "75deg",
+			},
+
+			colors: {
+				"light-nav": "#fbe9d0",
+			},
+		},
 	},
-	plugins: [],
+
+	plugins: [
+		plugin(function ({ matchUtilities, theme }) {
+			matchUtilities(
+				{
+					"text-shadow": value => ({
+						textShadow: value,
+					}),
+				},
+				{ values: theme("textShadow") },
+			);
+		}),
+	],
 };
