37e4c67e19fb89a1c82e424794022355a78410a0
laurelis <lauris-kairo@inbox.lv>
2024-10-17 12:49:56 +0300

    improved navbar style, need to fix responsiveness

--- front-end-react/src/components/Layout.jsx
+++ front-end-react/src/components/Layout.jsx
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { Outlet, useLocation } from "react-router-dom";
 import Navbar from "./navigation/Navbar";
 
 export default function Layout() {
-	let location = useLocation();
+	const [toggle, setToggle] = useState(false);
+
+	const handleToggleNav = () => {
+		setToggle(prev => !prev);
+	};
+
+	const handleCloseNav = () => {
+		setToggle(false);
+	};
+
 	return (
 		<>
-			<Navbar />
-			<Outlet />
+			<Navbar toggle={toggle} handleToggleNav={handleToggleNav} />
+			<div onClick={handleCloseNav}>
+				<Outlet />
+			</div>
 		</>
 	);
 }
