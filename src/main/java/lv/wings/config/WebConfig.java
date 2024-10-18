d48333a1a1a1a6cd2f0ad21b73632b470df58d46
Dep <viktors.idk@gmail.com>
2024-10-18 19:42:12 +0300

    Temporary react fix

--- src/main/java/lv/wings/config/WebConfig.java
+++ src/main/java/lv/wings/config/WebConfig.java
@@ -2,6 +2,7 @@ package lv.wings.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
@@ -15,5 +16,19 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOrigins("http://localhost:3000") 
                 .allowedMethods("*") 
                 .allowCredentials(true);
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/{spring:(?!static|api)[^\\.]*}")
+                .setViewName("forward:/index.html");
+        registry.addViewController("/*/{spring:(?!static|api)[^\\.]*}")
+                .setViewName("forward:/index.html");
+        registry.addViewController("/*/*/{spring:(?!static|api)[^\\.]*}")
+                .setViewName("forward:/index.html");
+        registry.addViewController("/*/*/*/{spring:(?!static|api)[^\\.]*}")
+                .setViewName("forward:/index.html");
+        registry.addViewController("/*/*/*/*/{spring:(?!static|api)[^\\.]*}")
+                .setViewName("forward:/index.html");
     }
 }
