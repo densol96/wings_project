7498481a2e618e89ccc255476e859221c6e8ad1f
laurelis <lauris-kairo@inbox.lv>
2024-10-21 11:48:27 +0300

    Testing jwt tokken, spring security, doesnt work right now

--- front-end-react/src/App.jsx
+++ front-end-react/src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate} from "react-router-dom";
+import {jwtDecode} from 'jwt-decode';
 import MainPage from "./components/MainPage";
 import News from "./components/news/News";
 import Shop from "./components/Shop";
@@ -11,11 +12,41 @@ import AdminDashboard from "./components/admin/AdminDashboard";
 import AdminNews from "./components/admin/AdminNews";
 import AdminProducts from "./components/admin/AdminProducts";
 import SingleNews from "./components/news/SingleNews";
+import LoginPage from "./components/LoginPage";
 
 /// Izveido react router, lai pareizi darbotos SPA
 function App() {
+
+
+	const isAuthenticated = () => {
+		const token = localStorage.getItem('token');
+		if (!token) return false;
+	
+		try {
+			const decodedToken = jwtDecode(token);
+	
+		
+			if (decodedToken.exp * 1000 < Date.now()) {
+				return false; // Token has expired
+			}
+	
+			/*
+			const userRoles = decodedToken.roles || decodedToken.authorities;
+			if (userRoles && userRoles.includes('ROLE_ADMIN')) {
+				return true;
+			}
+			*/
+	
+			return true; 
+		} catch (error) {
+			console.error("Invalid token", error);
+			return false;
+		}
+	};
+
 	return (
 		<>
+		
 			<BrowserRouter>
 				<Routes>
 					<Route path="/" element={<Layout />}>
@@ -28,7 +59,10 @@ function App() {
 						<Route path="/about" element={<About />} />
 						<Route path="/contacts" element={<Contacts />} />
 
-						<Route path="/admin" element={<AdminLayout />}>
+						
+						<Route path="/admin" element={isAuthenticated() ? <AdminLayout /> : <Navigate to={"/login"}/>}>
+
+
 							<Route index element={<AdminDashboard />} />
 
 							<Route
@@ -39,6 +73,7 @@ function App() {
 						</Route>
 						<Route path="*" element={<PageNotFound />} />
 					</Route>
+					<Route path="/login" element={<LoginPage />} />
 				</Routes>
 			</BrowserRouter>
 		</>
