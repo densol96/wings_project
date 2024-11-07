ff4d0c02e064e90df444b6873e879e267ba99de0
Dep <viktors.idk@gmail.com>
2024-11-07 16:07:36 +0200

    totally not finished

---
+++ src/main/java/lv/wings/mail/impl/MailSenderImpl.java
@@ -0,0 +1,33 @@
+package lv.wings.mail.impl;
+
+import java.util.Properties;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.stereotype.Service;
+
+import lv.wings.mail.MailSender;
+
+@Service
+public class MailSenderImpl implements MailSender{
+
+	@Override
+	@Bean
+	public JavaMailSender getJavaMailSender() throws Exception{
+		JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+		mailSender.setHost("smtp.gmail.com");
+		mailSender.setPort(587);
+		
+		mailSender.setUsername("my.gmail@gmail.com");
+		mailSender.setPassword("password");
+		
+		Properties props = mailSender.getJavaMailProperties();
+		props.put("mail.transport.protocol", "smtp");
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.debug", "true");
+		
+		return mailSender;
+	}
+}
