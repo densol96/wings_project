6a145a995196a2026951f8f06d6fd1b3a52ba024
Dep <viktors.idk@gmail.com>
2024-12-23 12:56:44 +0200

    Forgot to save push this file

--- src/main/java/lv/wings/repo/IDeliveryTypeRepo.java
+++ src/main/java/lv/wings/repo/IDeliveryTypeRepo.java
@@ -1,10 +1,11 @@
 package lv.wings.repo;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import lv.wings.model.DeliveryType;
 
-public interface IDeliveryTypeRepo extends CrudRepository<DeliveryType, Integer>{
+public interface IDeliveryTypeRepo extends CrudRepository<DeliveryType, Integer>, PagingAndSortingRepository<DeliveryType, Integer>{
 
     DeliveryType findByTitle(String title);
 
