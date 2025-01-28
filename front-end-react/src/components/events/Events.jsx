62cde64bc6bda93c776486c5052ce1e774563523
laurelis24 <23kairlaur@venta.lv>
2025-01-28 03:57:31 +0200

    single event view,  implemented galery, small changes

--- front-end-react/src/components/events/Events.jsx
+++ front-end-react/src/components/events/Events.jsx
@@ -1,219 +1,201 @@
 import Title from "../Title";
 import { useAllData } from "../../hooks/dataHooks";
 import LoadingSpinner from "../assets/LoadingSpinner";
-import { useNavigate, useLocation, Link } from 'react-router-dom';
+import { useNavigate, useLocation, Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import ErrorMessage from "../errors/ErrorMessage";
+import convertToLocalTime from "../../utils/convertToLocalTime";
 
 export default function News() {
-    const location = useLocation();
+	const location = useLocation();
 
 	const [events, setEvents] = useState([]);
 	const [error, setError] = useState(null);
 	const [loading, setLoading] = useState(false);
 	const [totalPages, setTotalPages] = useState(0);
-	//const { data, loading, error } = useAllData("http://localhost:8080/api/events");
+	const IMAGES_URL = `http://localhost:8080/images/`;
 
-
-	
 	const getQueryParams = () => {
-        const params = new URLSearchParams(location.search);
+		const params = new URLSearchParams(location.search);
 
-
-        return {
-            page: parseInt(params.get('page')) || 1,
-            sort: params.get('sort') || 'startDate',
-        };
-    };
+		return {
+			page: parseInt(params.get("page")) || 1,
+			sort: params.get("sort") || "startDate",
+		};
+	};
 
 	useEffect(() => {
-        const { page, sort } = getQueryParams();
-        
-        const fetchEvents = async () => {
-            try {
-				setLoading(true)
-                const response = await axios.get('http://localhost:8080/api/events', {
-                    params: { page, sort }
-                });
-                
-        if (!response.data.result){
-          setError(response.data.message);
-        } else {
-          setEvents(response.data.result.content); 
-				  setTotalPages(response.data.result.page.totalPages - 1);
-        }
-      
-				
-            } catch (error) {
-				setError(error)
-                console.error('Error fetching events:', error);
-            } finally{
+		const { page, sort } = getQueryParams();
+
+		const fetchEvents = async () => {
+			try {
+				setLoading(true);
+				const response = await axios.get("http://localhost:8080/api/events", {
+					params: { page, sort },
+				});
+
+				if (!response.data.result) {
+					setError(response.data.message);
+				} else {
+					setEvents(response.data.result.content);
+					setTotalPages(response.data.result.page.totalPages - 1);
+				}
+			} catch (error) {
+				setError(error);
+				console.error("Error fetching events:", error);
+			} finally {
 				setLoading(false);
 			}
-        };
+		};
 
-        fetchEvents();
-    }, [location.search]); // Re-run when URL changes
+		fetchEvents();
+	}, [location.search]); // Re-run when URL changes
 
-
-	
-
-	
-	 if (loading) {
+	if (loading) {
 		return <LoadingSpinner />;
-	} 
+	}
 
-	
-
-	 if (error) {
-		return <ErrorMessage error={error}/>
-	} 
+	if (error) {
+		return <ErrorMessage error={error} />;
+	}
 
 	return (
 		<>
 			<Title title={"Jaunumi - Pasākumi"} />
 			<main>
-				 <div className="flex flex-col justify-center overflow-hidden bg-gray- py-6 sm:py-12">
-  <div className="mx-auto max-w-screen-xl px-4 w-full">
-    <div className="grid w-full sm:grid-cols-2 xl:grid-cols-4 gap-6">
-									{events.map((n, i) => {
-										return <NewComponent key={n.startDate + i} data={n} />;
-									})} 
-						
-				      
+				<div className="flex flex-col justify-center overflow-hidden bg-gray- py-6 sm:py-12">
+					<div className="mx-auto max-w-screen-xl px-4 w-full">
+						<div className="grid w-full sm:grid-cols-2 xl:grid-cols-4 gap-6">
+							{events.map((n, i) => {
+								return (
+									<Link to={`show/${n.id}`}>
+										<EventComponent key={n.startDate + i} url={IMAGES_URL} data={n} />
+									</Link>
+								);
+							})}
 						</div>
-  </div>
-</div>
-		
-     {totalPages > 1 && <Pagination  getQueryParams={getQueryParams} totalPages={totalPages}  />}
-      
-     
+					</div>
+				</div>
 
+				{totalPages > 1 && <Pagination getQueryParams={getQueryParams} totalPages={totalPages} />}
 			</main>
 		</>
 	);
 }
 
-function NewComponent({ data }) {
-	let startDate = new Date(data.startDate).toLocaleDateString("lv-LV");
-	let endDate = new Date(data.endDate).toLocaleDateString("lv-LV");
+function EventComponent({ url, data }) {
 	let description = data.description.slice(0, 100);
-
 
 	return (
 		<div className="flex flex-col shadow-md rounded-xl overflow-hidden hover:shadow-lg hover:-translate-y-1 transition-all duration-300 max-w-sm">
-       
-        <div className="h-auto overflow-hidden">
-          <div className="h-44 overflow-hidden relative">
-            <img src="https://picsum.photos/400/400" alt=""/>
-          </div>
-        </div>
-        <div className="bg-white py-4 px-3">
-          <h3 className="text-xs mb-2 font-medium">{data.title}</h3>
-          <div>
-            <p className="text-xs text-gray-400">
-            {startDate} - {endDate}
-			</p>
-          </div>
-          <div>
-            <p className="text-xs text-gray-400 break-words">
-              {description}...
-			</ p>
-          </div>
-        </div>
-
-      </div>
-
-		
+			<div className="h-auto overflow-hidden">
+				<div className="h-44 overflow-hidden relative">
+					<img
+						src={
+							data.eventPictures.length > 0
+								? `${url}${data.eventPictures[0].referenceToPicture}`
+								: `https://www.allianceplast.com/wp-content/uploads/no-image.png`
+						}
+						alt="Attēls"
+					/>
+				</div>
+			</div>
+			<div className="bg-white py-4 px-3">
+				<h3 className="text-xs mb-2 font-medium">{data.title}</h3>
+				<div>
+					<p className="text-xs text-gray-400">
+						{convertToLocalTime(data.startDate)} - {convertToLocalTime(data.endDate)}
+					</p>
+				</div>
+				<div>
+					<p className="text-xs text-gray-400 break-words">{description}...</p>
+				</div>
+			</div>
+		</div>
 	);
 }
 
+function Pagination({ getQueryParams, totalPages }) {
+	const { page } = getQueryParams();
 
-
-function Pagination({getQueryParams, totalPages}){
-	const {page } = getQueryParams();
-	
 	const pageNumbers = [];
-    const range = 2; 
-    const startPage = Math.max(1, page - range);
-    const endPage = Math.min(totalPages, page + range);
+	const range = 2;
+	const startPage = Math.max(1, page - range);
+	const endPage = Math.min(totalPages, page + range);
 
 	for (let i = startPage; i <= endPage; i++) {
-        pageNumbers.push(i);
-    }
-	
-  return (
-    <div className="z-0 flex items-center justify-center border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
-      <div className="flex flex-1 justify-between sm:hidden">
-		<Link
-		
-          to={`/events?page=${page - 1}`}
-          className={`${page === 1 ? "pointer-events-none opacity-50" : ""} relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50`}
-        >
-          Iepriekšējā
-        </Link>
-       
-        <Link
-		
-          to={`/events?page=${page + 1}`}
-          className={`${page === totalPages ? "pointer-events-none opacity-50" : ""} relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50`}
-        >
-          Nākamā
-        </Link>
-      </div>
-      <div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-center">
-        <div>
-          <nav aria-label="Pagination" className="isolate inline-flex -space-x-px rounded-md shadow-xs">
-		  
-            {/* Previous Page */}
-            <Link
-                to={`?page=${page - 1}`}
-                className={`relative inline-flex items-center px-4 py-2 text-sm font-semibold ${page === 1 ? 'text-gray-500 cursor-not-allowed' : 'text-indigo-600 hover:bg-indigo-600 hover:text-white'}`}
-                aria-disabled={page === 1}
-                onClick={(e) => page === 1 && e.preventDefault()}
-            >
-                &laquo; Iepriekšējā
-            </Link>
+		pageNumbers.push(i);
+	}
 
-			{page === totalPages && <Link
-                to={`?page=${1}`}
-                className={`relative inline-flex items-center px-4 py-2 text-sm font-semibold ${page === 0 ? 'text-gray-500 cursor-not-allowed' : 'text-indigo-600 hover:bg-indigo-600 hover:text-white'}`}
-                onClick={(e) => page === 1 && e.preventDefault()}
-            >
-                1
-            </Link>}
+	return (
+		<div className="z-0 flex items-center justify-center border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
+			<div className="flex flex-1 justify-between sm:hidden">
+				<Link
+					to={`/events?page=${page - 1}`}
+					className={`${page === 1 ? "pointer-events-none opacity-50" : ""} relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50`}
+				>
+					Iepriekšējā
+				</Link>
 
-            {/* Page Numbers */}
-            {pageNumbers.map((pageNumber) => (
-                <Link
-                    key={pageNumber}
-                    to={`?page=${pageNumber}`}
-                    className={`relative inline-flex items-center px-4 py-2 text-sm font-semibold ${
-                        page === pageNumber
-                            ? 'bg-indigo-600 text-white'
-                            : 'text-gray-900 ring-1 ring-gray-300 ring-inset hover:bg-gray-50'
-                    }`}
-                    aria-current={page === pageNumber ? 'page' : undefined}
-                >
-                    {pageNumber}
-                </Link>
-            ))}
+				<Link
+					to={`/events?page=${page + 1}`}
+					className={`${page === totalPages ? "pointer-events-none opacity-50" : ""} relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50`}
+				>
+					Nākamā
+				</Link>
+			</div>
+			<div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-center">
+				<div>
+					<nav aria-label="Pagination" className="isolate inline-flex -space-x-px rounded-md shadow-xs">
+						{/* Previous Page */}
+						<Link
+							to={`?page=${page - 1}`}
+							className={`relative inline-flex items-center px-4 py-2 text-sm font-semibold ${page === 1 ? "text-gray-500 cursor-not-allowed" : "text-indigo-600 hover:bg-indigo-600 hover:text-white"}`}
+							aria-disabled={page === 1}
+							onClick={e => page === 1 && e.preventDefault()}
+						>
+							&laquo; Iepriekšējā
+						</Link>
 
-            {/* Next Page */}
-            <Link
-                to={`?page=${page + 1}`}
-                className={`relative inline-flex items-center px-4 py-2 text-sm font-semibold ${page === totalPages ? 'text-gray-500 cursor-not-allowed' : 'text-indigo-600 hover:bg-indigo-600 hover:text-white'}`}
-                aria-disabled={page === totalPages}
-                onClick={(e) => page === totalPages && e.preventDefault()}
-            >
-                Nākamā &raquo;
-            </Link>
-          </nav>
-        </div>
-      </div>
-    </div>
-  )
+						{page === totalPages && (
+							<Link
+								to={`?page=${1}`}
+								className={`relative inline-flex items-center px-4 py-2 text-sm font-semibold ${page === 0 ? "text-gray-500 cursor-not-allowed" : "text-indigo-600 hover:bg-indigo-600 hover:text-white"}`}
+								onClick={e => page === 1 && e.preventDefault()}
+							>
+								1
+							</Link>
+						)}
+
+						{/* Page Numbers */}
+						{pageNumbers.map(pageNumber => (
+							<Link
+								key={pageNumber}
+								to={`?page=${pageNumber}`}
+								className={`relative inline-flex items-center px-4 py-2 text-sm font-semibold ${
+									page === pageNumber
+										? "bg-indigo-600 text-white"
+										: "text-gray-900 ring-1 ring-gray-300 ring-inset hover:bg-gray-50"
+								}`}
+								aria-current={page === pageNumber ? "page" : undefined}
+							>
+								{pageNumber}
+							</Link>
+						))}
+
+						{/* Next Page */}
+						<Link
+							to={`?page=${page + 1}`}
+							className={`relative inline-flex items-center px-4 py-2 text-sm font-semibold ${page === totalPages ? "text-gray-500 cursor-not-allowed" : "text-indigo-600 hover:bg-indigo-600 hover:text-white"}`}
+							aria-disabled={page === totalPages}
+							onClick={e => page === totalPages && e.preventDefault()}
+						>
+							Nākamā &raquo;
+						</Link>
+					</nav>
+				</div>
+			</div>
+		</div>
+	);
 }
-
-
--- front-end-react/src/components/events/SingleEvent.jsx
+++ front-end-react/src/components/events/SingleEvent.jsx
@@ -3,6 +3,15 @@ import { useAsyncError, useParams } from "react-router-dom";
 import axios from "axios";
 import LoadingSpinner from "../assets/LoadingSpinner";
 import ErrorMessage from "../errors/ErrorMessage";
+import convertToLocalTime from "../../utils/convertToLocalTime";
+import LightGallery from "lightgallery/react";
+import "lightgallery/css/lightgallery.css";
+import "lightgallery/css/lg-zoom.css";
+import "lightgallery/css/lg-thumbnail.css";
+
+// Import plugins
+import lgZoom from "lightgallery/plugins/zoom";
+import lgThumbnail from "lightgallery/plugins/thumbnail";
 
 export default function SingleEvent() {
 	const { id } = useParams();
@@ -10,55 +19,101 @@ export default function SingleEvent() {
 	const [error, setError] = useState(null);
 	const [loading, setLoading] = useState(false);
 
-	
-		useEffect(() => {
-			
-			const fetchEvent = async () => {
-				try {
-					setLoading(true)
-					let response = await axios.get(`http://localhost:8080/api/events/show/${id}`);
-					
-					 
-					 if (!response.data.result){
-						setError(response.data.message)
-					 } else {
-						setEvent(response.data.result); 
-					 }
-					
-					
-				} catch (error) {
-					setError(error)
-					
-				} finally{
-					setLoading(false);
+	useEffect(() => {
+		const fetchEvent = async () => {
+			try {
+				setLoading(true);
+				let response = await axios.get(`http://localhost:8080/api/events/show/${id}`);
+
+				// console.log(response.data.result.eventPictures[0].referenceToPicture)
+				if (!response.data.result) {
+					setError(response.data.message);
+				} else {
+					setEvent(response.data.result);
 				}
-			};
- 
-			fetchEvent();
 
-			
-		}, []);
-	
+				console.log(response.data.result);
+			} catch (error) {
+				setError(error);
+			} finally {
+				setLoading(false);
+			}
+		};
 
-	if (loading) {
+		fetchEvent();
+	}, []);
+
+	if (loading || !event) {
 		return <LoadingSpinner />;
-	} 
-	
+	}
+
 	if (error) {
-		return <ErrorMessage  error={error} />
+		return <ErrorMessage error={error} />;
 	}
 
 	return (
-		<>
-			{event && (
-				<>
-					<h2 className="text-2xl font-bold text-center">{event.title}</h2>
-					<h2 className="text-2xl font-bold text-center">{event.startDate}</h2>
-					<h2 className="text-2xl font-bold text-center">{event.endDate}</h2>
-					<h2 className="text-2xl font-bold text-center">{event.location}</h2>
-					<h2 className="text-2xl font-bold text-center">{event.description}</h2>
-				</> 
-			)}
-		</>
+		<section>
+			<div className="mx-auto w-full max-w-7xl px-5 py-12 md:px-10 md:py-16 lg:py-20">
+				<div className="grid gap-12 sm:gap-20 lg:grid-cols-2">
+					<div className="flex flex-col items-start gap-2">
+						<p className="text-sm text-gray-500 sm:text-xl">{event.eventCategory.title}</p>
+						<h1 className="mb-6 text-4xl font-bold md:text-4xl lg:mb-8">{event.title}</h1>
+						<p className="text-sm text-gray-500 sm:text-xl">{event.description}</p>
+						<div className="mb-8 mt-8 h-px w-full bg-black"></div>
+						<div className="mb-6 flex flex-col gap-2 text-sm text-gray-500 sm:text-base lg:mb-8">
+							<p>
+								<strong>Sākuma datums: {convertToLocalTime(event.startDate)}</strong>
+							</p>
+							<p>
+								<strong>Beigu datums: {convertToLocalTime(event.endDate)}</strong>
+							</p>
+						</div>
+					</div>
+					<div className="min-h-[400px] relative max-h-[800px] overflow-hidden rounded-md shadow-lg bg-gray-100 group">
+						{event.eventPictures.length <= 0 ? (
+							<NoImageComponent />
+						) : (
+							<Gallery images={event.eventPictures} />
+						)}
+
+						{event.eventPictures.length > 0 && (
+							<span className="pointer-events-none absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 p-6 bg-slate-100 rounded-lg opacity-60 font-bold text-xl group-hover:opacity-90 transition-opacity">
+								{`1/${event.eventPictures.length}`}
+							</span>
+						)}
+					</div>
+				</div>
+			</div>
+		</section>
+	);
+}
+
+function NoImageComponent() {
+	return <img src="https://www.allianceplast.com/wp-content/uploads/no-image.png" alt="Attēla-nav" />;
+}
+
+function Gallery({ images }) {
+	const IMAGES_URL = `http://localhost:8080/images/`;
+
+	return (
+		<div className="App">
+			<LightGallery speed={500} plugins={[lgZoom, lgThumbnail]} closable={true} zoomFromOrigin={true}>
+				{images &&
+					images.map((image, index) => (
+						<a
+							key={index}
+							href={`${IMAGES_URL}${image.referenceToPicture}`}
+							data-sub-html={image.title}
+							className={index > 0 ? "hidden" : ""}
+						>
+							<img
+								src={`${IMAGES_URL}${image.referenceToPicture}`}
+								alt={image.title}
+								className={index > 0 ? "hidden" : ""}
+							/>
+						</a>
+					))}
+			</LightGallery>
+		</div>
 	);
 }
